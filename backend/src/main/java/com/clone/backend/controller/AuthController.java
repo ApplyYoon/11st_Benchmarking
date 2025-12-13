309580602b09f5e69599e52ad6ba9df352a3faec
swatchka <yoonsung2007@gmail.com>
2025-12-13 19:49:01 +0900

    19:48

--- backend/src/main/java/com/clone/backend/controller/AuthController.java
+++ backend/src/main/java/com/clone/backend/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
